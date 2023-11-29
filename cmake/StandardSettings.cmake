b1caff975f6b90c208045c4bdb3d7c93034d4784
ArcherWn <mhqjeaock0649@gmail.com>
2023-11-29 23:35:04 +0800

    Enable building tests in CMake configuration

--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -4,7 +4,7 @@
 option(${PROJECT_NAME}_BUILD_SHARED_LIBS "Build ${PROJECT_NAME} as a shared library" ON)
 option(${PROJECT_NAME}_BUILD_HEADERS_ONLY "Build only the headers of ${PROJECT_NAME}" OFF)
 option(${PROJECT_NAME}_BUILD_EXAMPLES "Build the examples of ${PROJECT_NAME}" OFF)
-option(${PROJECT_NAME}_BUILD_TESTS "Build the tests of ${PROJECT_NAME}" OFF)
+option(${PROJECT_NAME}_BUILD_TESTS "Build the tests of ${PROJECT_NAME}" ON)
 
 #
 # 編譯器設定
