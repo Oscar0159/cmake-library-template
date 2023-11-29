af28b871c86629d691ffd30b600062cc26ffb92e
ArcherWn <mhqjeaock0649@gmail.com>
2023-11-29 23:16:22 +0800

    Add CMake files for compiler warnings, sources and headers, and standard settings

---
+++ cmake/StandardSettings.cmake
@@ -0,0 +1,12 @@
+#
+# 專案設定
+#
+option(${PROJECT_NAME}_BUILD_SHARED_LIBS "Build ${PROJECT_NAME} as a shared library" ON)
+option(${PROJECT_NAME}_BUILD_HEADERS_ONLY "Build only the headers of ${PROJECT_NAME}" OFF)
+option(${PROJECT_NAME}_BUILD_EXAMPLES "Build the examples of ${PROJECT_NAME}" OFF)
+option(${PROJECT_NAME}_BUILD_TESTS "Build the tests of ${PROJECT_NAME}" OFF)
+
+#
+# 編譯器設定
+#
+option(${PROJECT_NAME}_WARNINGS_AS_ERRORS "Treat warnings as errors" OFF)
