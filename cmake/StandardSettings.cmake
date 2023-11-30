df10ac1dfa2bba7960d0d0389fa7cbf11f419895
ArcherWn <mhqjeaock0649@gmail.com>
2023-12-01 02:47:58 +0800

    Add options for using Google Test as the test framework

--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -5,7 +5,14 @@ option(${PROJECT_NAME}_BUILD_SHARED_LIBS "Build ${PROJECT_NAME} as a shared libr
 option(${PROJECT_NAME}_BUILD_HEADERS_ONLY "Build only the headers of ${PROJECT_NAME}" OFF)
 option(${PROJECT_NAME}_BUILD_EXAMPLES "Build the examples of ${PROJECT_NAME}" OFF)
 option(${PROJECT_NAME}_BUILD_TESTS "Build the tests of ${PROJECT_NAME}" ON)
+
 option(${PROJECT_NAME}_USE_ALT_NAMES "Use alternative names for the project, such as naming the include directory all lowercase." ON)
+
+#
+# 專案測試設定
+#
+option(${PROJECT_NAME}_USE_GOOGLE_TEST "Use Google Test as the test framework" ON)
+option(${PROJECT_NAME}_FETCH_GOOGLE_TEST "Fetch Google Test automatically" ON)
 
 #
 # 編譯器設定
